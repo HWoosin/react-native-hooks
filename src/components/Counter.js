a5599b136b6fa4723a3e6b39682ee92f8a73a876
woosin <ws1340cc@naver.com>
2026-03-08 23:19:12 +0900

    useState에 함수로 인자를 전달해보았습니다.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,11 +10,21 @@ const StyledText = styled.Text`
 const Counter = () => {
     const [count, setCount] = useState(0);
 
-    return (
+    return (// 가장 기본적으로 변경될 상태값을 전달하는 방법, 지금부턴 함수를 넣을것임
         <>
             <StyledText>count: {count}</StyledText>
-            <Button title="+" onPress={() =>{
+            {/* <Button title="+" onPress={() =>{
                 setCount(count + 1);
+            }}/> */}
+            {/* <Button title="+" onPress={() =>{
+                setCount(count + 1);
+                setCount(count + 1); //세터함수를 두개 줬는데 1씩 오름;; 비동기로 동작하기 때문 -> 상태값이 바로 변경되지 않는다.
+                console.log(`count: ${count}`);
+            }}/> */}
+            <Button title="+" onPress={() =>{
+                setCount(prevCount => prevCount + 1); //함수로 인자 전달
+                setCount(prevCount => prevCount + 1);
+                console.log(`count: ${count}`);
             }}/>
              <Button title="-" onPress={() =>{
                 setCount(count - 1);
