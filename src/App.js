cf0b12253de8fc1b62e4bdb5e54280c0f92aba0c
woosin <ws1340cc@naver.com>
2026-03-15 00:22:11 +0900

    마운트, 언마운트 상황을 테스트 했습니다

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native';
 import Counter from './components/Counter';
 import Form from './components/Form';
+import { useState } from 'react';
+import Button from './components/Button';
 
 const Container = styled.View`
   flex: 1;
@@ -12,10 +14,17 @@ const Container = styled.View`
 `;
 
 export default function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  
   return (
     <Container>
       {/* <Counter /> */}
-      <Form/>
+      {/* <Form/> */}
+      <Button
+        title={isVisible ? 'Hide': 'Show'}
+        onPress={() => setIsVisible(prev =>!prev)}  
+      />
+      {isVisible && <Form/>}
     </Container>
     
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -23,11 +23,17 @@ const StyledText = styled.Text`
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
 
-    useEffect(() => {//useEffect는 컴포넌트가 랜더링될 때마다 원하는 작업이 실행하도록 설정 가능.
-        // 첫번째 파라미터로 전달된 함수는 조건을 만족할때 호출, 두번째 파라미터로 전달되는 배열을 이용해 함수가 호출되는 조건설정 가능.
-        console.log(`name: ${name}, email: ${email}\n`);
-    });
-
+    // useEffect(() => {//useEffect는 컴포넌트가 랜더링될 때마다 원하는 작업이 실행하도록 설정 가능.
+    //     // 첫번째 파라미터로 전달된 함수는 조건을 만족할때 호출, 두번째 파라미터로 전달되는 배열을 이용해 함수가 호출되는 조건설정 가능.
+    //     console.log(`name: ${name}, email: ${email}\n`);
+    // },[email]);//해당 상태관리하는 변수전달. -> email이 변경될때마다 동작함.
+    // useEffect(() => {
+    //     console.log('\n==== Form Component Mount ====\n');
+    // },[]);//마운트 -> 처음 렌더링 될때만 호출됨
+    useEffect(() => {
+        console.log('\n==== Form Component Mount ====\n');
+        return () => console.log('\n==== Form Component UnMount ====\n');
+    },[]);//언마운트 -> 화면이 사라질 정리함수 실행
     return (
       <>
         <StyledText>Name: {name} </StyledText>
